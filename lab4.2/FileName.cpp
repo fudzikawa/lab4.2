6b3bbbb6b040244276511f31cd7756ba9856dec8
Fudzikawa <fdzkwww@gmail.com>
2023-12-14 21:10:28 +0200

    first commit

---
+++ lab4.2/FileName.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = 3 * x + 2;
+		if (x <= -3.5)
+			B  = x * x + 4;
+		else
+			if (x > 1)
+				B = cos(3 * x) - sin(x);
+			else
+				B = 4.95 * x * x + atan2(2 * x + 3.5, 5);
+		y = A + B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
